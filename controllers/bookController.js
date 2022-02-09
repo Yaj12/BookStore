c32775c8731d061ea6abce836ea1e21d795bedc1
9p6b <9p6b@fresnou.org>
2022-02-09 08:55:40 -0800

    Edit and Add are having the same problem but I am basically on slide 33 of part 1 on this project

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,19 +1,55 @@
 const {Book} = require('../models')
+const genres = ['Harem', 'Action Fiction', 'Romance', 'Thriller', 'Comedy'].sort();
 
 //view all
 module.exports.viewAll = async function(req,res){
-    const books = Book.findAll();
+    const books = await Book.findAll();
     res.render('books/view_all', {books});
 }
 
-//book profile
+//profile
+module.exports.viewProfile= async function(req,res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('books/profile', {book});
+}
 
 //render add form
+module.exports.renderAddForm = function(req, res) {
+    const book = {
+        title: '',
+        publisher: '',
+        genre: genres[0],
+        pages: '',
+        image: '',
+        description: ''
+    };
+    res.render('book/add', {book, genres});
+};
+
 
 //add
 
 //render edit form
+module.exports.renderEditForm = async function(req, res){
+    const book = await Book.findByPk(req.params.id);
+    res.render('book/edit', {book, genres});
+}
 
 //update
+module.exports.updateBook = async function(req, res) {
+    await Book.update( {
+        title: req.body.title,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        page_amount: req.body.pages,
+        cover_image: req.body.image,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/books/profile/${req.params.id}`);
+};
 
 //delete
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/books', bookController.viewAll);
+router.get('/books/profile/:id', bookController.viewProfile);
+router.get('/books/edit/:id', bookController.renderEditForm);
+router.get('/books/add', bookController.renderAddForm);
 module.exports = router;
